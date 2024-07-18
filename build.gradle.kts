7ad1836595594badc2f389112f8742864ffd060c
boris <borissiobko@gmail.com>
2024-07-18 19:12:42 +0300

    Update slf4j dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,9 +15,9 @@ dependencies {
 
     testImplementation("io.rest-assured:rest-assured:5.4.0")
 
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
 
-    testImplementation("org.slf4j:slf4j-simple:2.0.0")
+    testImplementation("io.goodforgod:slf4j-simple-logger:2.0.0")
 
     testCompileOnly ("org.projectlombok:lombok:1.18.32")
     testAnnotationProcessor ("org.projectlombok:lombok:1.18.32")
