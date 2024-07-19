ca500b43878f1257384708f2ab774e9293d815d4
boris <borissiobko@gmail.com>
2024-07-19 23:58:57 +0300

    Create update user job and check changes test

--- src/test/java/tests/put/UpdateUserJobAndCheckJobTest.java
+++ src/test/java/tests/put/UpdateUserJobAndCheckJobTest.java
@@ -9,20 +9,21 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import utils.UsersUtils;
 import tests.ApiTest;
 import utils.EndPoints;
+import utils.UsersUtils;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static utils.Specifications.*;
+import static utils.Specifications.requestSpec;
+import static utils.Specifications.responseSpecOK200;
 import static utils.UserRequestGenerator.*;
 
-public class UpdateUserNameAndCheckNameTest extends ApiTest {
+public class UpdateUserJobAndCheckJobTest extends ApiTest {
 
-    private static final Logger log = LoggerFactory.getLogger(UpdateUserNameAndCheckNameTest.class);
+    private static final Logger log = LoggerFactory.getLogger(UpdateUserJobAndCheckJobTest.class);
 
-    private static final UpdateUserRequest updateUserNameRequest = getUpdateUserNameRequest(); ;
+    private static final UpdateUserRequest updateUserJobRequest = getUpdateUserJobRequest(); ;
 
     private static CreateUserResponse createdUser;
 
@@ -35,23 +36,23 @@ public class UpdateUserNameAndCheckNameTest extends ApiTest {
     @Tag("PUT")
     @Test
     public void updateUserNameAndCheckNameTest() {
-        log.info("Создаем пользователя {}", updateUserNameRequest.getName());
-        UpdateUserResponse updateUserNameResponse = given()
+        log.info("Создаем пользователя {}", updateUserJobRequest.getJob());
+        UpdateUserResponse updateUserJobResponse = given()
                 .spec(requestSpec(BASE_URL))
-                .body(updateUserNameRequest)
+                .body(updateUserJobRequest)
                 .pathParam("id", createdUser.getId())
                 .put(EndPoints.updateUsers)
                 .then()
                 .spec(responseSpecOK200())
                 .log().all()
                 .extract().as(UpdateUserResponse.class);
-        assertEquals(updateUserNameRequest.getName(), updateUserNameResponse.getName(),
-                "Не обновилось значение имени пользователя");
+        assertEquals(updateUserJobRequest.getJob(), updateUserJobResponse.getJob(),
+                "Не обновилось значение занятости пользователя");
     }
 
     @AfterEach
     public void cleanUp() {
-        log.info("Удаляем созданного пользователя с именем {}", createdUser.getName());
+        log.info("Удаляем созданного пользователя с занятостью {}", createdUser.getJob());
         UsersUtils.deleteUser(createdUser.getId());
     }
 }
